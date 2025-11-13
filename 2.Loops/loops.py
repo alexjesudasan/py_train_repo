bb1259843e562a5b39f1f083dceae38d8ae628eb
alexjesudasan <reach.chosen@gmail.com>
2025-11-13 15:32:37 +0530

    Modified to check

--- 2.Loops/loops.py
+++ 2.Loops/loops.py
@@ -1,24 +1,24 @@
 # Example: if-elif-else
 # Grading system
-# marks = int(input("Enter your marks: "))
-# if marks >= 90:
-#     print("Grade: A")
-# elif marks >= 75:
-#     print("Grade: B")
-# elif marks >= 50:
-#     print("Grade: C")
-# else:
-#     print("Grade: Fail")
-# print("End of program")
+marks = int(input("Enter your marks: "))
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 75:
+    print("Grade: B")
+elif marks >= 50:
+    print("Grade: C")
+else:
+    print("Grade: Fail")
+print("End of program")
 
 # #for loop
-rows = 5
-for i in range(1, rows+1):
-    print("*" * i)
-
-# #while loop
-count = 5
-print("Count down starts")
-while count > 0:
-    print(f"{count} minutes" )
-    count -= 1
+# rows = 5
+# for i in range(1, rows+1):
+#     print("*" * i)
+#
+# # #while loop
+# count = 5
+# print("Count down starts")
+# while count > 0:
+#     print(f"{count} minutes" )
+#     count -= 1
