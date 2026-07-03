a1fc218e54378afee9d42638eeda5a4c212c55cf
alexjesudasan <reach.chosen@gmail.com>
2026-07-03 17:18:04 +0530

    Added new directory

---
+++ 1.Variables_Operators/Hands-on/myfirst_program.py
@@ -0,0 +1,47 @@
+#1
+print("Hello World");
+
+#2
+name="David"
+age=25
+print("My name is ",name);
+print(f"My name is {name} of age {age}");
+
+#3
+num=str(7);
+print(num+num);
+print(num*3)
+
+#4
+a,b,c=3,4,5
+print(a,b,c)
+
+#5
+stmt=False
+a="Banana is a Vegetable"
+print (f"Statement {a} is {stmt}")
+
+#6
+print("\nHappy\nBirthday",name)
+
+#7
+print("Bible Quote :\"Fear of the Lord\"")
+
+#8
+print("  *  ")
+print(" *** ")
+print("*****")
+
+#9
+principal = 1000   # Initial amount
+rate = 5           # Interest rate in percent
+time = 3           # Time in years
+interest = (principal * rate * time) / 100
+print("The Simple Interest is:", interest)
+
+#10
+score1 = 85
+score2 = 90
+score3 = 95
+average = (score1 + score2 + score3) / 3
+print("The average test score is:", average)
